d763c3dc98f4b8b6d252ece0266ef61a5b06874c
Myroslava Martyniuk <martyniuk.myroslava@gmail.com>
2024-02-26 22:22:23 +0200

    [Initial commit] simple calculator

---
+++ app.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+def substract(a, b):
+    return a - b
+
+if __name__ == '__main__':
+    num1 = int(input('Enter first number: '))
+    num2 = int(input('Enter second number: '))
+    operator = input('Enter the operator + or -')
+
+    if operator == '+':
+        print(f"The result of addition is: {add(num1, num2)}")
+    elif operator == '-':
+        print(f"The result of substraction is: {substract(num1, num2)}")
+    else:
+        print('Invalid operator')
