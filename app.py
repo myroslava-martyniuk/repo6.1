cf58fd759763af7cf5ba3595cc5c627c7a8d8d99
Myroslava Martyniuk <martyniuk.myroslava@gmail.com>
2024-02-26 23:30:49 +0200

    [Hopefully final commit] calculator alltogether

--- app.py
+++ app.py
@@ -5,11 +5,15 @@ def substract(a, b):
 
 def multiplication(a, b):
     return a * b
+def division(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
 
 if __name__ == '__main__':
     num1 = int(input('Enter first number: '))
     num2 = int(input('Enter second number: '))
-    operator = input('Enter the operator +, - or *')
+    operator = input('Enter the operator +, -, * or /: ')
 
     if operator == '+':
         print(f"The result of addition is: {add(num1, num2)}")
@@ -17,5 +21,7 @@ if __name__ == '__main__':
         print(f"The result of substraction is: {substract(num1, num2)}")
     elif operator == '*':
         print(f"The result of multiplication is: {multiplication(num1, num2)}")
+    elif operator == '/':
+        print(f"The result of division is: {division(num1, num2)}")    
     else:
         print('Invalid operator')
